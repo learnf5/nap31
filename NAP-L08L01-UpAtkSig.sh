f1014e4a3dc400a4fe13bf57753c1eb5dc96de64
learnf5 <99698691+learnf5@users.noreply.github.com>
2024-02-13 17:36:34 -0500

    Create NAP-L08L01-UpAtkSig.sh

---
+++ NAP-L08L01-UpAtkSig.sh
@@ -0,0 +1,19 @@
+# update lab environment
+sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+sudo ssh nginx mkdir --parents /etc/app_protect/conf/nap.d
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab08/{nginx,nap}.conf
+sudo scp /tmp/nginx.conf                                    nginx:/etc/nginx/
+sudo scp /tmp/nap.conf                                      nginx:/etc/nginx/conf.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab02/{nginx,nap}.orig
+sudo scp /tmp/nginx.orig                                    nginx:/etc/nginx/
+sudo scp /tmp/nap.orig                                      nginx:/etc/nginx/conf.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab06/simplify_requests.json
+sudo scp /tmp/simplify_requests.json                        nginx:/etc/app_protect/conf/nap.d/
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab07/my-default-policy.json
+sudo scp /tmp/my-default-policy.json                        nginx:/tmp/my-default-policy.json
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab07/policy_{transparent,viol_{http,evasion}}.json
+sudo scp /tmp/policy_{transparent,viol_{http,evasion}}.json nginx:/etc/app_protect/conf/nap.d
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab08/{policy_viol_filetype.json,my-filetypes.txt,local-policy.json}
+sudo scp /tmp/policy_viol_filetype.json                     nginx:/etc/app_protect/conf/nap.d
+sudo scp /tmp/my-filetypes.txt                              nginx:/etc/app_protect/conf/
+sudo scp /tmp/local-policy.json                             nginx:/etc/app_protect/conf/nap.d/
