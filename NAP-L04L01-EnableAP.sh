40d5638169571de5be650ca3c6f3b08ba4048a69
learnf5 <99698691+learnf5@users.noreply.github.com>
2024-02-12 19:25:56 -0500

    Create NAP-L04L01-EnableAP.sh

---
+++ NAP-L04L01-EnableAP.sh
@@ -0,0 +1,8 @@
+# update lab environment
+sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab01/{nginx,nap}.conf
+sudo scp /tmp/nginx.conf nginx:/etc/nginx/
+sudo scp /tmp/nap.conf   nginx:/etc/nginx/conf.d/ 
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab02/{nginx,nap}.orig
+sudo scp /tmp/nginx.orig nginx:/etc/nginx/
+sudo scp /tmp/nap.orig   nginx:/etc/nginx/conf.d/
