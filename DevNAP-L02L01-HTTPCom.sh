d020c0120617a8495eadf75bf7cdac3fa5da85d3
learnf5 <99698691+learnf5@users.noreply.github.com>
2024-02-23 12:31:51 -0500

    Update DevNAP-L02L01-HTTPCom.sh

--- DevNAP-L02L01-HTTPCom.sh
+++ DevNAP-L02L01-HTTPCom.sh
@@ -3,7 +3,7 @@ set -x
 PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
-sudo ssh nginx rm /etc/nginx/conf.d/default.conf
+sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab01/{nginx,nap}.conf
 sudo scp /tmp/nginx.conf nginx:/etc/nginx/
 sudo scp /tmp/nap.conf   nginx:/etc/nginx/conf.d/ 
