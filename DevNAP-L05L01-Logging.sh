dc7d34368287817b7cf8ec0fb4c27b47e9091e88
learnf5 <99698691+learnf5@users.noreply.github.com>
2024-02-23 12:04:59 -0500

    Update DevNAP-L05L01-Logging.sh

--- DevNAP-L05L01-Logging.sh
+++ DevNAP-L05L01-Logging.sh
@@ -1,3 +1,7 @@
+# enable debugging
+set -x
+PS4='+$(date +"%T.%3N"): '
+
 # update lab environment
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab05/nginx.conf
