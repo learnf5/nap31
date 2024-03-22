b6c32ce2662b9a1dda3d2698cef659768368186b
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-03-22 13:47:58 -0700

    Update DevNAP-L02L01-HTTPCom.sh

--- DevNAP-L02L01-HTTPCom.sh
+++ DevNAP-L02L01-HTTPCom.sh
@@ -4,9 +4,9 @@ PS4='+$(date +"%T.%3N"): '
 
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab01/{nginx,nap}.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab01/{nginx,nap}.conf
 sudo scp /tmp/nginx.conf nginx:/etc/nginx/
 sudo scp /tmp/nap.conf   nginx:/etc/nginx/conf.d/ 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab02/{nginx,nap}.orig
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab02/{nginx,nap}.orig
 sudo scp /tmp/nginx.orig nginx:/etc/nginx/
 sudo scp /tmp/nap.orig   nginx:/etc/nginx/conf.d/
