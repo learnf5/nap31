cb0b7a9aaf636713ce0ae638d74a280037de8889
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-03-22 13:47:21 -0700

    Update DevNAP-L07L01-Tuning.sh

--- DevNAP-L07L01-Tuning.sh
+++ DevNAP-L07L01-Tuning.sh
@@ -5,16 +5,16 @@ PS4='+$(date +"%T.%3N"): '
 # update lab environment
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 sudo ssh nginx mkdir --parents /etc/app_protect/conf/nap.d
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab07/nginx.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab07/nginx.conf
 sudo scp /tmp/nginx.conf nginx:/etc/nginx/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab06/nap.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab06/nap.conf
 sudo scp /tmp/nap.conf   nginx:/etc/nginx/conf.d/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab02/{nginx,nap}.orig
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab02/{nginx,nap}.orig
 sudo scp /tmp/nginx.orig nginx:/etc/nginx/
 sudo scp /tmp/nap.orig   nginx:/etc/nginx/conf.d/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab06/simplify_requests.json
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab06/simplify_requests.json
 sudo scp /tmp/simplify_requests.json nginx:/etc/app_protect/conf/nap.d/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab07/my-default-policy.json
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab07/my-default-policy.json
 sudo scp /tmp/my-default-policy.json nginx:/tmp/my-default-policy.json
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap/main/lab07/policy_{transparent,viol_{http,evasion,filetype}}.json
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab07/policy_{transparent,viol_{http,evasion,filetype}}.json
 sudo scp /tmp/policy_{transparent,viol_{http,evasion,filetype}}.json nginx:/etc/app_protect/conf/nap.d/
