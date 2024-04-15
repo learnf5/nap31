0e661aaac5894478cd16c0ea157f2eca4e1fe822
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-04-15 15:02:20 -0700

    Update DevNAP-L09L01-PreventInfoLeak.sh

--- DevNAP-L09L01-PreventInfoLeak.sh
+++ DevNAP-L09L01-PreventInfoLeak.sh
@@ -5,7 +5,7 @@ PS4='+$(date +"%T.%3N"): '
 # update lab environment
 sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 sudo ssh nginx mkdir --parents /etc/app_protect/conf/nap.d
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab08/{nginx,nap}.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab09/{nginx,nap}.conf
 sudo scp /tmp/nginx.conf                                    nginx:/etc/nginx/
 sudo scp /tmp/nap.conf                                      nginx:/etc/nginx/conf.d/
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab02/{nginx,nap}.orig
