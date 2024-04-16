dd17688506095d297e808f332062c98c7b847ccb
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-04-15 18:52:16 -0700

    Update DevNAP-L10L01-DosAtk.sh

--- DevNAP-L10L01-DosAtk.sh
+++ DevNAP-L10L01-DosAtk.sh
@@ -24,9 +24,10 @@ sudo scp /tmp/my-filetypes.txt                             nginx:/etc/app_protec
 sudo scp /tmp/local-policy.json                            nginx:/etc/app_protect/conf/nap.d/
 sudo scp /tmp/app-protect-security-updates.key             nginx:/home/student/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab10/{nginx,nap}.WAF
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab10/{{nginx,nap}.WAF,policy_data_guard.json}
 sudo scp /tmp/nginx.WAF                                    nginx:/etc/nginx/
 sudo scp /tmp/nap.WAF                                      nginx:/etc/nginx/conf.d/
+sudo scp /tmp/policy_data_guard.json                       nginx:/etc/app_protect/conf/nap.d/
 
 #On student workstation/jump box
 curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/lab10/GoodTraffic.sh
