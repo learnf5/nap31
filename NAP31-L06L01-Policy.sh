68938de787db2462fd829dc16b399fa3e9b1d228
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2024-06-04 13:49:22 -0700

    Update NAP31-L06L01-Policy.sh

--- NAP31-L06L01-Policy.sh
+++ NAP31-L06L01-Policy.sh
@@ -5,14 +5,14 @@ PS4='+$(date +"%T.%3N"): '
 # update lab environment
 sudo ssh nginx test -e /etc/nginx/conf.d/default.conf && sudo ssh nginx rm /etc/nginx/conf.d/default.conf
 sudo ssh nginx mkdir --parents /etc/app_protect/conf/nap.d
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab06/{nginx,nap}.conf
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap31/main/lab06/{nginx,nap}.conf
 sudo scp /tmp/nginx.conf nginx:/etc/nginx/
 sudo scp /tmp/nap.conf   nginx:/etc/nginx/conf.d/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab02/{nginx,nap}.orig
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap31/main/lab02/{nginx,nap}.orig
 sudo scp /tmp/nginx.orig nginx:/etc/nginx/
 sudo scp /tmp/nap.orig   nginx:/etc/nginx/conf.d/
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab06/simplify_requests.json
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap31/main/lab06/simplify_requests.json
 sudo scp /tmp/simplify_requests.json nginx:/etc/app_protect/conf/nap.d/
 
-curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/devnap/main/lab01/hosts
+curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nap31/main/lab01/hosts
 sudo scp /tmp/hosts nginx:/etc/hosts
