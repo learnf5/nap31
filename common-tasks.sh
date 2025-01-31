83931ed5df051f56ef2f4ec2110412d38a44d82e
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-01-31 15:16:52 -0800

    Update common-tasks.sh
    Updated for new license that expire March 2, 2025

--- common-tasks.sh
+++ common-tasks.sh
@@ -24,8 +24,8 @@ chmod +x /home/student/Desktop/Lab_Guide.desktop
 
 # install nginx license
 set +x
-curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/nginx-repo.{crt,key,jwt}
-echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/nginx-repo.{crt,key,jwt}
+curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-2-2025/nginx-repo.{crt,key,jwt}
+echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-2-2025/nginx-repo.{crt,key,jwt}
 set -x
 until sudo scp /tmp/nginx-repo.{crt,key} nginx:/etc/ssl/nginx/ || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-repo.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
