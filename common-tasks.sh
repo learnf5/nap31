3c96223a2965e0aa95a581af92e6114a3cacd1b8
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-02-04 11:03:00 -0800

    Update common-tasks.sh
    updated names for crt & key to nginx-repo

--- common-tasks.sh
+++ common-tasks.sh
@@ -27,7 +27,8 @@ set +x
 curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-6-2025/nginx-one-eval.{crt,key,jwt}
 echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-6-2025/nginx-one-eval.{crt,key,jwt}
 set -x
-until sudo scp /tmp/nginx-one-eval.{crt,key} nginx:/etc/ssl/nginx/ || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
 
 # run this lab's specific tasks saved on GitHub
