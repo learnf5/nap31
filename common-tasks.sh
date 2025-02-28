ff4caa2077222d40ee0073707a5bb7245b1ded5a
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-02-28 11:23:08 -0800

    Update common-tasks.sh

--- common-tasks.sh
+++ common-tasks.sh
@@ -37,11 +37,11 @@ set +x
 #until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
 
 # ALL licenses from NGINX One - will it work for updating NAP DoS??? if not we have a problem since Salesforce no longer has NGINX Plus trial licenses. NOPE this license still doesn't work for me with NAP DoS!!!
-curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-30-2025/nginx-one.{crt,key,jwt}
-echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-30-2025/nginx-one.{crt,key,jwt}
+curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-30-2025/nginx-one-eval.{crt,key,jwt}
+echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-March-30-2025/nginx-one-eval.{crt,key,jwt}
 set -x
-until sudo scp /tmp/nginx-one.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
-until sudo scp /tmp/nginx-one.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
 until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
 
 # run this lab's specific tasks saved on GitHub
